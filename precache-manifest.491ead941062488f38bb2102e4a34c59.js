3aa78b024407ea36de8f9c349ef759d84d6de290
Billy Tjiptoning <8053504+bernardusbilly@users.noreply.github.com>
2020-08-28 02:19:20 +0700

    Updates

---
+++ precache-manifest.491ead941062488f38bb2102e4a34c59.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e65844738b561d5cbebf385610d72d94",
+    "url": "/index.html"
+  },
+  {
+    "revision": "058fd30046389540995c",
+    "url": "/static/css/main.1cd941b9.chunk.css"
+  },
+  {
+    "revision": "891d3e46982bc7d2a905",
+    "url": "/static/js/2.67952fbc.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.67952fbc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "058fd30046389540995c",
+    "url": "/static/js/main.61f03c1c.chunk.js"
+  },
+  {
+    "revision": "1f830289b4469fd59f65",
+    "url": "/static/js/runtime-main.e4975b63.js"
+  }
+]);
